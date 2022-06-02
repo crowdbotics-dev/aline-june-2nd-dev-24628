bdc435643b4b5d041bfab4b2afd129329a404891
Aline Tavares <alaisgomes@gmail.com>
2022-06-02 12:13:40 -0400

    Create screen with dependency

---
+++ screens/ResponsiveScreen/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text style={styles.myText}>Login</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  textWrapper: {
+    backgroundColor: 'lightpink',
+    height: hp('70%'), // 70% of height device screen
+    width: wp('80%'), // 80% of width device screen
+  },
+  myText: {
+    fontSize: hp('5%'), // End result looks like the provided UI mockup
+  },
+});
+
+export default Login;
