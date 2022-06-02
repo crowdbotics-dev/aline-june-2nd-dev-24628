8693e7b45ce54ac97fea5994046880daf00e94a8
Aline Tavares <aline@crowdbotics.com>
2022-06-02 12:16:34 -0400

    Crowdbotics Studio: Update app screens

--- screens/ResponsiveScreen/index.js
+++ screens/ResponsiveScreen/index.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const Login = () => {
-  return (
-    <View style={styles.container}>
+  return <View style={styles.container}>
       <View style={styles.textWrapper}>
         <Text style={styles.myText}>Login</Text>
       </View>
-    </View>
-  );
+    </View>;
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: {
+    flex: 1
+  },
   textWrapper: {
     backgroundColor: 'lightpink',
-    height: hp('70%'), // 70% of height device screen
-    width: wp('80%'), // 80% of width device screen
+    height: hp('70%'),
+    // 70% of height device screen
+    width: wp('80%') // 80% of width device screen
+
   },
   myText: {
-    fontSize: hp('5%'), // End result looks like the provided UI mockup
-  },
-});
+    fontSize: hp('5%') // End result looks like the provided UI mockup
 
+  }
+});
 export default Login;
